10eff969692c28c31647c1a3a8cb38ea54efd78e
Sebastián Urbina <129198429+Urbisin@users.noreply.github.com>
2024-10-06 20:24:25 -0500

    Delete Services directory

--- Services/Lo/models/Lo.js
+++
@@ -1,8 +0,0 @@
-
-const mongoose = require('mongoose');
-
-const LoSchema = new mongoose.Schema({
-  // Definición del esquema
-});
-
-module.exports = mongoose.model('Lo', LoSchema);
--- Services/Ma/models/Ma.js
+++
@@ -1,8 +0,0 @@
-
-const mongoose = require('mongoose');
-
-const MaSchema = new mongoose.Schema({
-  // Definición del esquema
-});
-
-module.exports = mongoose.model('Ma', MaSchema);
